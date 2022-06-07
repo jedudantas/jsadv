3da893ba15b9586c161690aa6a6f95ec3700a6de
J.Edu <jedu.f.dantas@gmail.com>
2022-06-07 11:40:22 -0300

    node

--- jsmodular/app.js
+++ jsmodular/app.js
@@ -1,10 +1,10 @@
-import { Car } from "./modules/car";
-import { Bike } from "./modules/bike";
+import { Car } from "./modules/car.js";
+import { Bike } from "./modules/bike.js";
 
 let carro = new Car();
 carro.andar();
-document.getElementById('main').innerHTML = 'carro diz: ${carro.velocidade} '
+document.getElementById('main').innerHTML = `carro diz: ${carro.velocidade} `
 
 let bike = new Bike();
 bike.andar();
-document.getElementById('main').innerHTML = 'bike diz: ${bike.velocidade}'
+document.getElementById('main').innerHTML = `bike diz: ${bike.velocidade} `
---
+++ projetonode/mymodules/app.mjs
@@ -0,0 +1,6 @@
+import moment from "moment";
+
+moment.locale("pt-BR");
+now = moment();
+
+console.log(now);
