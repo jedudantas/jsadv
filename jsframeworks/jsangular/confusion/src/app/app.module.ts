ca54cfe2bc1de41d5937899db3e95ec80a9d56ed
J.Edu <jedu.f.dantas@gmail.com>
2022-07-01 18:37:38 -0300

    Basic Routing

--- jsframeworks/jsangular/confusion/src/app/app.module.ts
+++ jsframeworks/jsangular/confusion/src/app/app.module.ts
@@ -9,12 +9,20 @@ import { MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list'
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-import { MatCardModule } from '@angular/material/card'
-import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component'
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button';
+
+import { DishService } from './services/dish.service';
+
+import { HomeComponent } from './home/home.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +30,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +43,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
 
   ],
   providers: [DishService],
