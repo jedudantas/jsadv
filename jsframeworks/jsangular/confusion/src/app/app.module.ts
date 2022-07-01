cf86f0166905add02b628fbade4c1619b635c2a1
J.Edu <jedu.f.dantas@gmail.com>
2022-07-01 18:13:26 -0300

    Basic Services

--- jsframeworks/jsangular/confusion/src/app/app.module.ts
+++ jsframeworks/jsangular/confusion/src/app/app.module.ts
@@ -12,12 +12,17 @@ import { MenuComponent } from './menu/menu.component';
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component'
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +35,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component'
     MatButtonModule
 
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
