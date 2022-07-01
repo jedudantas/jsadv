b26c26aa54576c64f1320a0ed2f1effb2f086266
J.Edu <jedu.f.dantas@gmail.com>
2022-07-01 20:10:46 -0300

    Single Page App pt1

--- jsframeworks/jsangular/confusion/src/app/app.module.ts
+++ jsframeworks/jsangular/confusion/src/app/app.module.ts
@@ -18,7 +18,7 @@ import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button';
 
 import { DishService } from './services/dish.service';
-
+import { PromotionService } from './services/promotion.service';
 import { HomeComponent } from './home/home.component';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
@@ -47,7 +47,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule
 
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
